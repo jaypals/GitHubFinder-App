0bea7747ad027398325579d0638a58ae9fd61fd4
mcauleystage <>
2023-03-13 23:41:42 +0800

    adding navbar/footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
 	return (
-		<div>
-			<h1 className='text-xl'>Hello World</h1>
-		</div>
+		<BrowserRouter>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+				<main className='container mx-auto px-3 pb-12'>Content</main>
+				<Footer />
+			</div>
+		</BrowserRouter>
 	);
 }
 
