c5d2251f7104fc71e107f00d7b66b72b825a7b02
mcauleystage <>
2023-03-16 01:41:03 +0800

    adding initial github api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import UserResult from "../components/users/UserResult";
 
 function Home() {
 	return (
-		<div>
-			<h1 className='text-6xl'>Welcome</h1>
-		</div>
+		<>
+			{/* SEARCH COMPONENT */}
+			<UserResult />
+		</>
 	);
 }
 
