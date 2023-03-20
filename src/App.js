29cd794040660b3321426a1624f4ead62f3297d7
mcauleystage <>
2023-03-21 02:05:58 +0800

    addded alert component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import Footer from "./components/layout/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertConext";
 
 function App() {
 	return (
 		<GithubProvider>
-			<BrowserRouter>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
-					<main className='container mx-auto px-3 pb-12'>
-						<Routes>
-							<Route exact path='/' element={<Home />} />
-							<Route exact path='/About' element={<About />} />
-							<Route exact path='/*' element={<NotFound />} />
-						</Routes>
-					</main>
-					<Footer />
-				</div>
-			</BrowserRouter>
+			<AlertProvider>
+				<BrowserRouter>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
+						<main className='container mx-auto px-3 pb-12'>
+							<Alert />
+							<Routes>
+								<Route exact path='/' element={<Home />} />
+								<Route exact path='/About' element={<About />} />
+								<Route exact path='/*' element={<NotFound />} />
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</BrowserRouter>
+			</AlertProvider>
 		</GithubProvider>
 	);
 }
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,16 +1,18 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertConext";
 
 function UserSearch() {
 	const [text, setText] = useState("");
 
 	const { users, searchUsers, clearUsers } = useContext(GithubContext);
+	const { setAlert } = useContext(AlertContext);
 
 	const handleChange = (e) => setText(e.target.value);
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (text === "") {
-			alert("Please enter something");
+			setAlert("Please enter something", "error");
 		} else {
 			searchUsers(text);
 			setText("");
